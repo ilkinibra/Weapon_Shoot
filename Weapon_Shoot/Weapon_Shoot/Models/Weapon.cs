d1fae038a26cbcfb5fe8a0405b8ea35517c81d40
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2022-05-14 12:06:26 +0400

    Add files via upload

---
+++ Weapon_Shoot/Weapon_Shoot/Models/Weapon.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon_Shoot.Models
+{
+    class Weapon
+    {
+        public int charger;
+        public int nowcharger;
+        public string mode;
+        public int remainbullet;
+        public int chg = 0;
+        public Weapon() { }
+        #region Charger
+        public int Charger
+        {
+            get
+            { return charger; }
+            set
+            {
+                if (value > 0 && value < 1000)
+                { charger = value; }
+                else
+                {
+                    charger = 0;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Firemode
+        public void ChangeFireMode()
+        {
+            if (this.chg % 2 == 0)
+            {
+                mode = "FullAuto";
+                this.chg++;
+            }
+            else
+            {
+                mode = "Single";
+                this.chg++;
+            }
+        }
+
+        public void FireModeFullAuto()
+        {
+            if (this.nowcharger > 0)
+            {
+                if (mode == "FullAuto")
+                {
+                    this.nowcharger = 0;
+                }
+                else
+                {
+                    this.nowcharger = this.nowcharger - 1;
+                }
+            }
+            else
+            {
+                this.nowcharger = 0;
+                Console.WriteLine("\nThe weapon cannot fire.Because there were no bullets.\nPlease Reload weapon.\n");
+            }
+        }
+        public void FireModeSingle()
+        {
+            if (this.nowcharger > 0)
+            {
+                this.nowcharger = this.nowcharger - 1;
+            }
+            else
+            {
+                this.nowcharger = 0;
+                Console.WriteLine("\nThe weapon cannot fire.Because there were no bullets.\nPlease Reload weapon.\n");
+            }
+        }
+
+        #endregion
+
+        #region Nowcharger
+        public int NowCharger()
+        {
+            this.nowcharger = this.charger;
+            return nowcharger;
+        }
+
+        #endregion
+
+        #region ChangeNowCharger
+        public int ChangeNowCharger
+        {
+            get
+            { return nowcharger; }
+            set
+            {
+                if (value >= 0 && value <= this.charger)
+                { nowcharger = value; }
+                else
+                {
+                    nowcharger = -1;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Reload
+        public void Reload()
+        {
+            if (this.nowcharger == this.charger)
+            {
+                Console.WriteLine("\nMagazine is Full !\n");
+            }
+            else
+            {
+                this.nowcharger = this.charger;
+                Console.WriteLine("\nWeapon is reloaded.\n");
+            }
+        }
+
+        #endregion
+
+        #region GetRemainBulletCount
+        public void GetRemainBulletCount()
+        {
+            this.remainbullet = this.charger - this.nowcharger;
+        }
+        #endregion
+    }
+}
---
+++ Weapon_Shoot/Weapon_Shoot/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Weapon_Shoot.Models;
+
+namespace Weapon_Shoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region FirstInformation
+
+            Weapon weapon = new Weapon();
+            Console.WriteLine("Please Enter The Magazine Size");
+        tryentercharger:
+            weapon.Charger = Convert.ToInt32(Console.ReadLine());
+            if (weapon.charger == 0)
+            {
+                Console.WriteLine("Wrong bullet count.\nPlease try again");
+                goto tryentercharger;
+            }
+            weapon.NowCharger();
+            weapon.mode = "Single";
+            Console.Clear();
+
+        #endregion
+
+        #region Menu
+
+        menu:
+            Console.WriteLine("0.To get information\n1.Shoot Single mode\n2.Shoot FullAuto mode\n3.Get remain bullet count\n4.Reload the weapon\n5.Change Fire Mode\n6.Edit the bullet\n7.Close program");
+            int menu = Convert.ToInt32(Console.ReadLine());
+            switch (menu)
+            {
+                case 0:
+                    Console.WriteLine("");
+                    Console.WriteLine($"Magazine capacity: {weapon.charger}\n");
+                    Console.WriteLine($"Now magazine: {weapon.nowcharger}\n");
+                    Console.WriteLine($"Weapon mode: {weapon.mode}\n");
+                    goto menu;
+                case 1:
+                    Console.Clear();
+                    weapon.FireModeSingle();
+                    goto menu;
+                case 2:
+                    Console.Clear();
+                    weapon.FireModeFullAuto();
+                    goto menu;
+                case 3:
+                    Console.Clear();
+                    weapon.GetRemainBulletCount();
+                    Console.WriteLine($" Get Remain Bullet Count: {weapon.remainbullet}\n");
+                    goto menu;
+                case 4:
+                    Console.Clear();
+                    weapon.Reload();
+                    goto menu;
+                case 5:
+                    Console.Clear();
+                    weapon.ChangeFireMode();
+                    goto menu;
+                case 6:
+                    Console.Clear();
+                    Console.WriteLine("T - Change magazine capacity\nQ - Change now magazine bullet count\n= - Press any button to return");
+                    string menusection = Console.ReadLine();
+                    string editmenu = menusection.ToUpper();
+                    if (editmenu == "T")
+                    {
+                    tryeditcharger:
+                        weapon.Charger = Convert.ToInt32(Console.ReadLine());
+                        if (weapon.charger == 0)
+                        {
+                            Console.WriteLine("Wrong bullet count.\nPlease try again");
+                            goto tryeditcharger;
+                        }
+                        if (weapon.nowcharger >= weapon.charger)
+                        {
+                            weapon.NowCharger();
+                        }
+                        goto menu;
+                    }
+                    else if (editmenu == "Q")
+                    {
+                        Console.Clear();
+                    tryenternowcharger:
+                        weapon.ChangeNowCharger = Convert.ToInt32(Console.ReadLine());
+                        if (weapon.nowcharger == -1)
+                        {
+                            Console.WriteLine("Wrong bullet count.\nPlease try again");
+                            goto tryenternowcharger;
+                        }
+                        goto menu;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Nothing has changed");
+                        goto menu;
+                    }
+                default:
+                    Console.Clear();
+                    Console.WriteLine("===Thanks Goodbye \nProgram Exit!!!===");
+                    break;
+            }
+
+            #endregion
+        }
+    }
+}
